9da3f28611299556557e669f254614f5dd418880
dan@nerdi.com <dan@nerdi.com>
2013-11-27 08:26:54 +1100

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarks::Application.config.secret_key_base = '641aa1245fb255804bdbc4b581b182e09c8ace743f45e3f4ec00dd7fa19d4621835f79bd32771efe87b6e8f2e970c0696e9e70d4b073e83a6d9b27d2d591cfdc'
